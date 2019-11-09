4258b2e72faf56bccb53cac3984bfb79e892f530
Fernando <fernandocunha@outlook.com.br>
2019-11-09 01:23:20 -0200

    Email queue completed

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '98dee1b247d088',
+    pass: '8bc4741ec2b836',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
